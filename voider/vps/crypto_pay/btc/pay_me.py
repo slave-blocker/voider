60eef00b11ada820f0e4d467350e869f74a97999
slave-blocker <dollner@live.com>
2020-10-15 23:11:21 +0200

    15th commit

--- voider/vps/crypto_pay/btc/pay_me.py
+++ voider/vps/crypto_pay/btc/pay_me.py
@@ -6,7 +6,6 @@ import utils
 import pwd
 from shutil import copyfile
 
-os.rename("/root/pay_me.sh", "/root/crypto_pay/btc/pay_me.sh")
 
 os.rename("/var/sftp/newuser/address", "/var/sftp/newuser/processing")
 os.rename("/var/sftp/newuser/newuser", "/var/sftp/newuser/payment")
@@ -20,16 +19,19 @@ file.close()
 newuser = List[0]
 addr = List[1]
 
-with open("/var/sftp/newuser/address") as file:
+with open("/var/sftp/newuser/processing") as file:
     current_addr = file.read().splitlines()[0]
 file.close()
 
 payed = False
 count = 0 
 
+print(addr)
+print(current_addr)
+
 if addr == current_addr:
 
-    subprocess.run(["/root/crypto_pay/btc/electrum_daemon.sh"])
+    #subprocess.run(["/root/crypto_pay/btc/electrum_daemon.sh"])
 
     # sleep a bit, be polite to the daemon ...
 
@@ -38,6 +40,7 @@ if addr == current_addr:
     while not payed and count < 18 :
 
         with open("/root/crypto_pay/btc/confirmed", "w+") as confirmed:
+            subprocess.run(["/root/crypto_pay/btc/electrum_daemon.sh"])
             subprocess.run(["./electrum-3.3.8-x86_64.AppImage", "getaddressbalance", addr ], stdout=confirmed)
         confirmed.close()
 
@@ -48,20 +51,25 @@ if addr == current_addr:
         #print(confirm)
 
         # parse x:
-        y = json.loads(confirm)
-
+        try :
+            y = json.loads(confirm)
+            if float(y["confirmed"]) >= 0.001 :
+                payed = True
+            else :
+                time.sleep(600)
+        except Exception :
+            print("json load failed")
         #print(y["confirmed"])
-
-        if int(y["confirmed"]) >= 100000 :
-            payed = True
-        else :
-            time.sleep(600)
 
         count = count + 1
 
 #payed = True
 
 if payed :
+
+    with open("/root/crypto_pay/btc/payed", "w+") as pay:
+        pay.write(confirm)
+    pay.close()
 
     with open("/root/newuser") as file:
         List = file.readlines()
@@ -77,16 +85,19 @@ if payed :
     subprocess.run(["gpg", "--import", "/root/crypto_pay/btc/pubkey.asc" ])
 
     found = False
-    temp = newuser
-    count = -3
+    temp = newuser + '_0'
+    count = 0
     while not found:
-        count = count + 3
-        z=False
+        print(temp)
+        z = False
         for p in pwd.getpwall():
+            print(p[0])
             if(p[0] == temp):
-                z=True
                 print("User already exists")
+                z = True
+                break
         if z :
+            count = count + 3
             temp = newuser + '_' + str(count)
         else :
             found = True
@@ -111,12 +122,14 @@ if payed :
 
     print("pass2 : " + pas2)
 
-    access = [user_rw + '\n', pas1 + '\n', user_r + '\n', pas2 + '\n', folder]
-
-    try :
-        os.remove('/var/sftp/newuser/creds.gpg')
-    except Exception :
-        print("creds.gpg not there")
+    access = ["the ip of the vps : 192.236.162.238 \n",
+              "the read only user for the vps : " + user_r + '\n',
+              "the read only password for the vps : " + pas2 + '\n',
+              "the read write user for the vps : " + user_rw + '\n',
+              "the read write password for the vps : " + pas1 + '\n',
+              "the folder\'s name in the vps : " + folder + '\n',
+              "the port number that the vps is listening on : 6868"
+             ]
 
     with open("/root/crypto_pay/btc/creds", "w+") as creds:
         creds.writelines(access)
@@ -142,11 +155,12 @@ if payed :
 
     # now kill the daemon, because it won't work otherwise
 
-    subprocess.run(["/root/crypto_pay/btc/kill_daemon.sh"])
+    # subprocess.run(["/root/crypto_pay/btc/kill_daemon.sh"])
 
     time.sleep(3)
 
     with open("/root/crypto_pay/btc/newaddress", "w+") as newaddress:
+        subprocess.run(["/root/crypto_pay/btc/electrum_daemon.sh"])
         subprocess.run(["/root/crypto_pay/btc/electrum-3.3.8-x86_64.AppImage", "createnewaddress"], stdout=newaddress)
     newaddress.close() 
 
@@ -156,16 +170,19 @@ if payed :
 
     aList.append(newaddr)
 
-    L = json.dumps(aList)
+    try :
+        L = json.dumps(aList)
+    except Exception:
+        print("json dump failed")
 
     with open("/root/crypto_pay/btc/addresses", "w+") as addresses:
         addresses.write(L)
     addresses.close()
 
-    with open("/var/sftp/newuser/address", "w+") as address:
+    with open("/var/sftp/newuser/processing", "w+") as address:
         address.write(aList[0])
     address.close()
 
-os.rename("/root/crypto_pay/btc/pay_me.sh", "/root/pay_me.sh")
+
 os.rename("/var/sftp/newuser/processing", "/var/sftp/newuser/address")
 os.rename("/var/sftp/newuser/payment", "/var/sftp/newuser/newuser")
