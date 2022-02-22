c2905174019c4fdc512dd2cc84a419fa9c3772c8
slave_blocker <dollner@live.com>
2022-02-22 12:08:46 +0200

    16-th commit

--- voider/install.sh
+++ voider/install.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 find . -name "blub" -type f -delete
+cd ..
+mv voider/ ~/.config/
 sudo apt-get install tcpdump conntrack python3-paramiko bridge-utils
 curl -L https://install.pivpn.io | bash
 
